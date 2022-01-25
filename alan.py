9825b127af569973d29a8f8382c1f840aae8ea1f
chaosannals <chaosannals@outlook.com>
2022-01-25 17:59:23 +0800

    whoosh

--- alan.py
+++ alan.py
@@ -1,3 +1,56 @@
+import os
 from peewee import *
+from whoosh.index import create_in, open_dir
 from whoosh.fields import *
+from whoosh.analysis import StemmingAnalyzer
+from whoosh.qparser import QueryParser
 
+tester = Schema(
+    testname=ID(stored=True),
+    description=TEXT(stored=True),
+    tags=KEYWORD
+)
+
+ixt_dir = 'rt/tester'
+if not os.path.isdir(ixt_dir):
+    os.makedirs(ixt_dir)
+    ixt = create_in(ixt_dir, tester)
+else:
+    ixt = open_dir(ixt_dir)
+
+rt = ixt.reader()
+
+wt = ixt.writer()
+wt.add_document(
+    testname='aaaaa222',
+    description='aaaaa content',
+    tags=['aaa', 'bbb', ]
+)
+wt.commit()
+
+with ixt.searcher() as searcher:
+    query = QueryParser(
+        'description',
+        ixt.schema
+    ).parse('content')
+    rs = searcher.search(query)
+    print([r for r in rs])
+
+    q2 = QueryParser(
+        'tags',
+        ixt.schema
+    ).parse('aaa')
+    rs2 = searcher.search(q2)
+    print([r for r in rs2])
+
+
+####################
+class TestJobSchema(SchemaClass):
+    idtest = ID(stored=True)
+    texttest = TEXT(stored=True, analyzer=StemmingAnalyzer)
+    tags = KEYWORD
+
+ixtj_dir = 'rt/testjob'
+if not os.path.isdir(ixtj_dir):
+    os.makedirs(ixtj_dir)
+ixtj = create_in(ixtj_dir, TestJobSchema())
