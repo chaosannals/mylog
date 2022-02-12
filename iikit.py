7415267d11c60b37a726a2c790efdd913d1c6835
chaosannals <chaosannals@outlook.com>
2022-02-12 16:11:34 +0800

    优化可选

--- iikit.py
+++ iikit.py
@@ -6,6 +6,7 @@ from whoosh.fields import SchemaClass, FieldType, ID, STORED, TEXT, KEYWORD
 from whoosh.index import create_in, open_dir, FileIndex
 from jsondata import load_jsondata
 
+
 class DocRaw(SchemaClass):
     '''
 
@@ -32,7 +33,7 @@ class DocCollection:
         with self.indexer.writer() as w:
             w.optimize = True
             w.remove_field(f)
-    
+
     @staticmethod
     def _new_doc(data):
         return {
@@ -40,19 +41,20 @@ class DocCollection:
             'data': json.dumps(data, ensure_ascii=False),
         }
 
-    def add_doc(self, data):
+    def add_doc(self, data, **argkw):
         d = self._new_doc(data)
         with self.indexer.writer() as w:
-            w.optimize = True
+            for k, v in argkw.items():
+                setattr(w, k, v)
             w.add_document(**d)
 
-    def add_docs(self, rows):
+    def add_docs(self, rows, **argkw):
         with self.indexer.writer() as w:
-            w.optimize = True
+            for k, v in argkw.items():
+                setattr(w, k, v)
             for row in rows:
                 d = self._new_doc(row)
                 w.add_document(**d)
-
 
 
 def new_collection(name: str) -> DocCollection:
@@ -73,6 +75,7 @@ def test():
         print(f'load {p}')
         r = load_jsondata(p)
         dc.add_docs(r)
+    
 
 
 if '__main__' == __name__:
